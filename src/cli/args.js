7a6812587bf3674b6ff8bde49243799f6aa83e75
m208 <avcvet@yandex.ru>
2022-12-02 02:33:10 +0300

    feat: add CLI solution

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,21 @@
+import { createInterface } from 'readline';
+
+const rl = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 const parseArgs = () => {
-    // Write your code here 
+    console.log('Input your text: ');
+
+    rl.on('line', answer => {
+        rl.clearLine()
+        const arr = answer.split(' ')
+        
+        if (arr[0].includes('--') && arr[1]){
+            console.log(`${arr[0].replace('--', '')} is ${arr[1]}`)
+        } 
+    });
 };
 
 parseArgs();
