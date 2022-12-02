83f2fef959fe3484966a32d233e4a39fa853458b
m208 <avcvet@yandex.ru>
2022-12-02 20:11:34 +0300

    feat: add Streams solution

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,25 @@
+import { createWriteStream } from 'node:fs';
+import { stdin } from 'node:process';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const stream = createWriteStream(
+        path.join(__dirname, 'files', 'fileToWrite.txt')
+    );
+
+    stdin.on('data', (data) => {
+        stream.write(data);
+    });
+
+    process.on('SIGINT', () => {
+        stream.end();
+        process.exit();
+    });
 };
 
 await write();
