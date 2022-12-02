873d6ae30eea729ea3772b2397bd02a92466d37f
m208 <avcvet@yandex.ru>
2022-12-02 21:05:33 +0300

    feat: add ZIP solution

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,23 @@
+import { createGzip  } from 'node:zlib';
+import { createWriteStream, createReadStream } from 'node:fs';
+
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+    const source = createReadStream(
+        path.join(__dirname, 'files', 'fileToCompress.txt')
+    );
+    const destination = createWriteStream(
+        path.join(__dirname, 'files', 'archive.gz')
+    );
+
+    const gzip = createGzip();
+    source.pipe(gzip).pipe(destination);
 };
 
 await compress();
+
