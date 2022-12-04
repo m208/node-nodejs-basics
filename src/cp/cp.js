4fb4cb5594670888c37e61ad0922eccb032401b2
m208 <avcvet@yandex.ru>
2022-12-04 17:03:25 +0300

    feat: add CP solution

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,5 +1,17 @@
-const spawnChildProcess = async (args) => {
-    // Write your code here
-};
+import { fork } from 'child_process'
 
-spawnChildProcess();
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const script = path.join(__dirname, 'files', 'script.js');
+
+ export const spawnChildProcess = async (args) => {
+    fork(script, args);
+ }
+
+const args = ['a', 'b', 'c'];
+
+spawnChildProcess(args);
